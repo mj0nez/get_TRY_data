50c09eb9301208ad8df509dc2da64749ab8f1c97
mj0nez <mj0nez@fn.de>
2021-03-20 03:37:45 +0100

    Update data_extract_values_from_dwd_dataset.py

--- data_extract_values_from_dwd_dataset.py
+++ data_extract_values_from_dwd_dataset.py
@@ -1,83 +1,99 @@
-import os
 import pandas as pd
+from pathlib import Path
 
 # IMPORT parameters
-# PATH_SRC_FILES = os.getcwd()  # current working dir
-PATH_SRC_FILES = 'D:/Master Thesis/Modellentwicklung/TRY_DWD/product_wgs84_20200626__125716_Kiel_Stadtrand/TRY_543103100873/'
+# PATH_SRC_FILES = Path.cwd()  # current working dir
+PATH_SRC_FILES = 'D:/Master Thesis/Modellentwicklung/TRY_DWD/' \
+                 'product_wgs84_20200626__125716_Kiel_Stadtrand/' \
+                 'TRY_543103100873/'  # '<path of unpacked data set>'
 
 # EXPORT parameters
 # EXPORT_PREFIX = 'City_outskirts'
-EXPORT_PREFIX = 'City_center'       # replaces coordinates
+EXPORT_PREFIX = 'City_center'  # replaces coordinates
 
-# PATH_FOR_EXPORT = FOLDER_PATH_SRC_FILES   # to src folder
-PATH_FOR_EXPORT = 'D:/MATLAB_LoadFlow_Simulator/data_pv/'  # to Matlab folder
+PATH_FOR_EXPORT = PATH_SRC_FILES  # to src folder
+# PATH_FOR_EXPORT = '<your desired export location>'
 
 # VAL_TO_EXTRACT = 't'    # temperature
-# VAL_TO_EXTRACT = 'N'    # okta / Bedeckungsgrad
+# VAL_TO_EXTRACT = 'N'    # okta / degree of coverage
 # VAL_TO_EXTRACT = 'WG'   # wind speed
-# VAL_TO_EXTRACT = 'p'    # atmospharic pressure
+# VAL_TO_EXTRACT = 'p'    # atmospheric pressure
 # VAL_TO_EXTRACT = 'B'    # dir. solar irradiance (horiz. plane) [W/m^2]
 VAL_TO_EXTRACT = 'D'  # diff solar irradiance (horiz. plane) [W/m^2]
 
+EXPORT_FORMAT = '.txt'
 
-def readData(file_name, rows_to_skip):
+
+# EXPORT_FORMAT = '.csv'
+
+
+def readData(file):
     # column names of data set
-    value_names = ['RW', 'HW', 'MM', 'DD', 'HH', 't',
-                   'p', 'WR', 'WG', 'N', 'x', 'RF',
-                   'B', 'D', 'A', 'E', 'IL']
+    value_names = ['RW', 'HW', 'MM', 'DD', 'HH', 't', 'p', 'WR', 'WG', 'N',
+                   'x', 'RF', 'B', 'D', 'A', 'E', 'IL']
+
+    TRY_future = 'TRY2045_'  # identifier future data set
+    head_future = 36
+    head_normal = 34
+
+    # header to skip
+    rows_to_skip = head_future if TRY_future in file.name else head_normal
+
     # use python engine, because c engine cant handle 2 sep
-    return pd.read_csv(file_name,
+    return pd.read_csv(file,
                        skiprows=rows_to_skip,
                        engine='python',
-                       sep='\\s\\*',
+                       sep=r'\s+',
                        header=None,
                        names=value_names,
                        dtype=object)
 
 
-def exportCSV(data_frame_to_export,
-              export_path,
-              export_file_name):
-    # writes to new csv without index and header
-    full_file_name = export_path + export_file_name + '.txt'
-    # data_frame_to_export.to_csv(full_file_name,
-    #                             header=None,
-    #                             index=False)
-    print('...exported.\n')
+def getExportPath(export_path, file_prefix, file_name, val_name):
+    name_sep = '_'
+
+    name_string = file_name.split(name_sep)
+    export_file_name = name_sep.join([file_prefix,
+                                      name_string[0],
+                                      name_string[2],
+                                      val_name])  # w/o coordinates
+    return Path(export_path) / Path(export_file_name)
 
 
-def runExtraction(path_src_folder,
-                  path_for_export, file_prefix,
-                  val_to_extract):
-    # extraction only for source files
-    for file in os.listdir(path_src_folder):
-        file_ext_src = '.dat'
+def exportCSV(data_frame, export_name_path, export_format):
+    name = export_name_path.with_suffix(export_format)
+    data_frame.to_csv(name,
+                      header=None,
+                      index=False)
+    return
 
-        if file.endswith(file_ext_src):
-            print('Extracting data....for value: ', val_to_extract)
-            print('from: ', file)
 
-            TRY_future = 'TRY2045_'  # identifier future data set
+def runExtraction():
+    # local vars allow 
+    path_src_folder = PATH_SRC_FILES
+    file_ext_src = '.dat'
+    path_for_export = PATH_FOR_EXPORT
+    export_prefix = EXPORT_PREFIX
+    val_to_extract = VAL_TO_EXTRACT
+    export_format = EXPORT_FORMAT
 
-            if TRY_future in file:
-                skip_rows = 36  # header to skip
-            else:
-                skip_rows = 34  # for 2015 data
+    for file in list(Path(path_src_folder).glob('**/*' + file_ext_src)):
+        # creates PurePath objects from matching files in src folder
 
-            df = readData(path_src_folder + str(file), skip_rows)
-            df = df[val_to_extract]  # reduce data to column
+        print('Extracting data....for value: ', val_to_extract)
+        print('from: ', file.name)
 
-            name_export_file = str(file).split('_')  # rename output file
-            name_export_file = '_'.join([name_export_file[0],
-                                         name_export_file[2]])  # no coordinates
+        df = readData(file)
+        df = df[val_to_extract]  # reduce data to column
+        export_path = getExportPath(path_for_export,
+                                    export_prefix,
+                                    file,
+                                    val_to_extract)
+        exportCSV(df, export_path, export_format)
 
-            exportCSV(df, path_for_export, '_'.join([file_prefix,
-                                                     name_export_file,
-                                                     val_to_extract]))
+        print('...exported.\n')
+    return
 
 
 if __name__ == '__main__':
-    runExtraction(PATH_SRC_FILES,
-                  PATH_FOR_EXPORT,
-                  EXPORT_PREFIX,
-                  VAL_TO_EXTRACT)
+    runExtraction()
